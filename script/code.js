bda1e07e495bb108ad45d6d3b060f370a6f7fb63
Thabangx10 <Thabangx12@gmail.com>
2023-05-30 16:39:36 +0200

    ecom

---
+++ script/code.js
@@ -0,0 +1,23 @@
+// products
+
+// create an array of objects
+
+let products = [
+    {
+        id: '',
+        name: '',
+        description: '',
+        type: '',
+        price: '',
+        Image: ''
+    }
+];
+
+let result = document.getElementById('result')
+
+products.forEach ( product =>{
+    result.innerHTML += `
+    
+    
+    `
+})
