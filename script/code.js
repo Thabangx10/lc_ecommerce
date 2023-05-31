42cc78d37a2c697e6ec15beeea32ef87336da771
Thabangx10 <Thabangx12@gmail.com>
2023-05-31 11:10:11 +0200

    hey

--- script/code.js
+++ script/code.js
@@ -1,5 +1,5 @@
 // products
-
+let structure = document.querySelector('section#result .row:last-child')
 // create an array of objects
 
 let products = [
@@ -9,14 +9,32 @@ let products = [
         type: 'Home',
         price: 'R1 525 000',
         Image: 'https://i.postimg.cc/rwftpZsK/Equestria.jpg'
-    }
-];
+    },
+]
 
-let result = document.getElementById('result')
-
-products.forEach ( product =>{
+let result = document.querySelector('.product');
+products.forEach ((product) =>{
     result.innerHTML += `
-
-    
-    `
-})
+    <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="${product}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+   `
+});
